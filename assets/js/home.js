52172d4c251c16dca79f9ef7daa97133b3cc0e91
Aman Rai <arai24197@gmail.com>
2020-06-08 23:16:45 +0530

    Final Commit

--- assets/js/home.js
+++ assets/js/home.js
@@ -2,6 +2,8 @@ var checkboxs=document.querySelectorAll("#delete1 #task input");
 var taskDescpt=document.getElementsByClassName("task-des");
 var taskDate=document.getElementsByClassName("task-date");
 
+
+
 for(let i=0;i<checkboxs.length;i++)
 {
     checkboxs[i].addEventListener("click",function()
@@ -19,3 +21,35 @@ for(let i=0;i<checkboxs.length;i++)
     })
     
 }
+
+
+// ---- Display notification on adding/deleting tast----
+const notifyContainer = document.getElementById('notify'); 
+
+    function notification(type){ //--- type define is task deleting/Adding
+        if (type === 'error') {
+            notifyContainer.classList.remove('notify-success');
+            notifyContainer.classList.add('notify-error');
+            notifyContainer.innerText = "Task Deleted!!!";
+        }else if(type==='deleteAll'){
+            notifyContainer.classList.remove('notify-success');
+            notifyContainer.classList.add('notify-error');
+            notifyContainer.innerText = "Task's Deleted Successfully!!";
+        }else if (type === 'success') {
+            notifyContainer.classList.remove('notify-error');
+            notifyContainer.classList.add('notify-success');
+            var check=document.getElementById("form-description").value; //---- to check is input field is empty
+            if(check){
+                notifyContainer.innerText = "Task Added successfully";
+            }else{
+                notifyContainer.classList.remove('notify-success');
+                notifyContainer.classList.add('notify-error');
+                notifyContainer.innerText = "Input Field's can't be empty!!";
+            }
+        }
+        notifyContainer.style.display = 'block';
+
+        setTimeout(() => {    //---- show notification only for 1 sec
+            notifyContainer.style.display = 'none';
+        }, 1000)
+    }
